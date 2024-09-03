e98a62762157f224680a2bb035d815cbae972108
Dadit <dadit.utama@gmail.com>
2024-09-03 08:50:14 +0700

    Auth(fix): fixing checkAuth for token validation to hit user/auth endpoint and generate new accessToken

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,7 @@ import { cleanEnv, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
   PORT: str(),
+  HOST: str(),
   JWT_ACCESS_KEY: str(),
   JWT_REFRESH_KEY: str(),
   HOST_USERS_SERVICE: str(),
